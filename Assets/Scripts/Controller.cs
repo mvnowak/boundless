7308412e7368f64ebe9855f2ce479ea153e7840a
JakeGonder <gonderjake@gmail.com>
2024-06-10 12:07:13 +0200

    update prompt and script

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -9,21 +9,29 @@ public class Controller : MonoBehaviour
 
         string script = @"
             
-                public static void DynamicMethod(){
-                    GameObject[] spheres = GameObject.FindObjectsOfType<GameObject>().Where(obj => obj.name.StartsWith(""Sphere"")).ToArray();
-                    foreach (GameObject sphere in spheres) {
-                        float volume = CalculateVolume(sphere.transform.localScale);
-                        if (volume < 1) {
-                            GameObject.Destroy(sphere);
-                        }
-                    }
-                }
+public static void DynamicMethod(){
+    // Create a forest with a specified number of trees
+    int treeCount = 100;
+    float areaSize = 50f;
 
-                static float CalculateVolume(Vector3 scale) {
-                    return scale.x * scale.y * scale.z;
-                }
+    for (int i = 0; i < treeCount; i++)
+    {
+        GameObject tree = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        tree.name = 'Tree' + i;
+        tree.transform.position = new Vector3(Random.Range(-areaSize, areaSize), 0, Random.Range(-areaSize, areaSize));
+        tree.transform.localScale = new Vector3(1, Random.Range(5f, 15f), 1);
+
+        // Optionally, you can add tree leaves as spheres on top of the cylinders
+        GameObject leaves = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        leaves.transform.parent = tree.transform;
+        leaves.transform.localPosition = new Vector3(0, 1, 0);
+        leaves.transform.localScale = new Vector3(3, 3, 3);
+        leaves.GetComponent<Renderer>().material.color = Color.green;
+    }
+}
         ";
-
+        // replace single quotes with double quotes
+        script = script.Replace("'", "\"");
         try
         {
             Action dynamicMethod = runtimeCompiler.InterpretScript(script);
